c93da13f404d388a128ac370d18eeed0553cca13
Satyam Mishra <satyam.work.only@gmail.com>
2023-04-09 14:39:31 +0530

    added each login and removed picamera

--- app.py
+++ app.py
@@ -1,39 +1,56 @@
 from asyncio import sleep
 import msgTwilio
 import attachMail
+import subprocess
+import datetime
 from mpu6050 import mpu6050
 
 # Set up MPU6050 sensor
 sensor = mpu6050(0x68)
 
-# Set up PiCamera
-camera = picamera.PiCamera()
+def recordVideo(filename):
+    # Set the output filename and duration
+    duration = 5
 
-# Wait for any tilt or acceleration
-while True:
-    acceleration_data = sensor.get_accel_data()
-    gyroscope_data = sensor.get_gyro_data()
-    acceleration_threshold = 0.1  # Set acceleration threshold here
-    tilt_threshold = 1.0  # Set tilt threshold here
+    # Define the command to start the video recording
+    command = ['ffmpeg', '-t', str(duration), '-f', 'video4linux2', '-i', '/dev/video0>', filename]
+    # Start the recording process
+    process = subprocess.Popen(command)
 
-    if (abs(acceleration_data['x']) > acceleration_threshold or
-            abs(acceleration_data['y']) > acceleration_threshold or
-            abs(acceleration_data['z']) > acceleration_threshold or
-            abs(gyroscope_data['x']) > tilt_threshold or
-            abs(gyroscope_data['y']) > tilt_threshold or
-            abs(gyroscope_data['z']) > tilt_threshold):
-        print('Motion detected! Starting video recording...')
-        
-        # recording video for 5 second
-        
+    # Wait for the recording to finish
+    process.wait()
 
-        # sending recorded file with mail
-        filename = ''
-        attachMail.send(filename)
-        # sending msg
-        msgTwilio.sendMsg()
+def getAcclerAndGyro():
+    # Wait for any tilt or acceleration
+    while True:
+        acceleration_data = sensor.get_accel_data()
+        gyroscope_data = sensor.get_gyro_data()
+        acceleration_threshold = 0.1  # Set acceleration threshold here
+        tilt_threshold = 1.0  # Set tilt threshold here
 
-    else:
-        pass
+        if (abs(acceleration_data['x']) > acceleration_threshold or
+                abs(acceleration_data['y']) > acceleration_threshold or
+                abs(acceleration_data['z']) > acceleration_threshold or
+                abs(gyroscope_data['x']) > tilt_threshold or
+                abs(gyroscope_data['y']) > tilt_threshold or
+                abs(gyroscope_data['z']) > tilt_threshold):
+            print('Motion detected! Starting video recording...')
+            
 
-    sleep(5)
+            # recording video for 5 second
+            filename = 'video.avi'
+            recordVideo(filename=filename)
+
+            # sending recorded file with mail
+            attachMail.send(filename)
+
+            # sending msg
+            msgTwilio.sendMsg()
+
+        else:
+            pass
+
+        sleep(3)
+
+if __name__== '__main__':
+    getAcclerAndGyro()
